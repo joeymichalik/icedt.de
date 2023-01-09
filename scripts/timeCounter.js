ad4a3997e2509e46678b9639bc0fd68cb717b63d
Joey Michalik <75860520+joeymichalik@users.noreply.github.com>
2023-01-09 21:53:07 +0100

    Reworked Counter

--- scripts/timeCounter.js
+++ scripts/timeCounter.js
@@ -12,11 +12,38 @@ var myfunc = setInterval(function() {
     var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+
+    // Concatting from "1" to "01"
+    var seconds_str = "";
+    var minutes_str = "";
+    var hours_str = "";
+
+    if (hours < 10) {
+        hours_str = "0" + hours;
+    }
+    else {
+        hours_str = hours;
+    }
+
+    if (minutes < 10) {
+        minutes_str = "0" + minutes;
+    }
+    else {
+        minutes_str = minutes;
+    }
+
+    if (seconds < 10) {
+        seconds_str = "0" + seconds;
+    }
+    else {
+        seconds_str = seconds;
+    }
          
     // Result is output to the specific element
     document.getElementById("days").innerHTML = days + "d, "
     document.getElementById("hours").innerHTML = hours + "h, " 
     document.getElementById("mins").innerHTML = minutes + "m, " 
-    document.getElementById("secs").innerHTML = seconds + "s" 
+    document.getElementById("secs").innerHTML = seconds_str + "s" 
          
 }, 1000);
