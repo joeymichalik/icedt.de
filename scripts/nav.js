5f75ee34f4475661b516274d9f9e743fc8785a1c
Joey Michalik <75860520+joeymichalik@users.noreply.github.com>
2023-01-13 00:39:21 +0100

    Responsive Navbar

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,4 +1,4 @@
-const navbar = document.querySelector(".navbar");
+const navbar = document.querySelector(".primary-navbar");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
 navToggle.addEventListener("click", () => {
