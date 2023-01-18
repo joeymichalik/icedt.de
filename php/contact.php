2579479f840b2d05a0bc6e8e6f57dc322e6f94fe
Joey Michalik <75860520+joeymichalik@users.noreply.github.com>
2023-01-18 18:16:30 +0100

    Update contact.php

--- php/contact.php
+++ php/contact.php
@@ -49,7 +49,7 @@
 
         <?php
             if(isset($_POST["submit"])){
-            mail("contact@icedt.de", "Contact Form - " . $_POST["user-name"], "NEW MESSAGE PER CONTACT FORM:" . "\n\n" . $_POST["user-name"] . " - Mail: " . $_POST["user-mail"] . "\n\nMessage:\n" . $_POST["user-msg"]);
+            mail("contact@icedt.de", "Contact Form - " . $_POST["user-name"], "NEW MESSAGE PER CONTACT FORM:" . "\n\n" . $_POST["user-name"] . " - " . $_POST["user-mail"] . "\n\nMessage:\n" . $_POST["user-msg"]);
         }
         ?>
 
